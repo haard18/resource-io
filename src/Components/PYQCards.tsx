968c6eba62119c4636c442b7345d2d156c141ca7
haard18 <haardsolanki.itm@gmail.com>
2024-08-14 12:46:51 +0530

    Pyq updated

--- src/Components/PYQCards.tsx
+++ src/Components/PYQCards.tsx
@@ -6,7 +6,11 @@ import { AnimatedTooltipPreview } from "./Tooltip";
 export function PYQ() {
   return (
     <div className="mx-auto px-8 w-full bg-gradient-to-r from-black via-gray-900 to-black">
+      <h1 className="text-3xl text-center text-white pt-3">
+        Previous Year Questions Papers
+      </h1>
       <HoverEffect items={pyqs} />
+
       <AnimatedTooltipPreview/>
     </div>
 
